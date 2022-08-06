ed3ef1ca5f1aeef1c572b52683c913da9a038895
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-08-06 22:23:47 +0300

    Save comment to database

--- app.rb
+++ app.rb
@@ -31,12 +31,12 @@ configure do
 		content TEXT
 	)'
 
-		@db.execute 'CREATE TABLE IF NOT EXISTS Comments
+	@db.execute 'CREATE TABLE IF NOT EXISTS Comments
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		created_date DATE,
-		content TEXT
-		post_id integer
+		content TEXT,
+		post_id INTEGER
 	)'
 end
 
@@ -104,5 +104,22 @@ post '/details/:post_id' do
 	# получаем переменную из post-запроса	
   	content = params[:content]
 
-  	erb "You typed comment #{content} for post #{post_id}"
+  	# сохранение данных в БД
+
+  @db.execute 'insert into Comments
+  	(
+  		content,
+  		created_date,
+  		post_id
+  	)
+  		values
+  	(
+  		?, 
+  		datetime(),
+  		?
+  	)', [content, post_id]
+
+  	# перенаправление на страницу поста
+
+	redirect to('/details/' + post_id)
 end
